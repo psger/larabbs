efcffa8de23e9f0bb6d2b80eb97fec59095f4c08
Passenger <passengerlcw@gmail.com>
2018-02-03 14:39:32 +0000

    Finish user auth

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="" target="_blank">Passenger</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:passengerlcw@gmail.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:passengerlcw@gmail.com">联系</a></p>
     </div>
 </footer>
