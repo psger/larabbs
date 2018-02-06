ddfc873b08b92f05539b70a442404c4b8039f95c
Passenger <passengerlcw@gmail.com>
2018-02-06 13:33:12 +0000

    add userscontroller

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,6 @@ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 //Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
