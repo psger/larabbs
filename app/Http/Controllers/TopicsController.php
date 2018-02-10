ddb3d64b3fd1bd9118885504b97ea6407fdc332c
psger <psger@gmail.com>
2018-02-10 03:28:44 +0000

    User can create topic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use App\Models\Topic;
 use Illuminate\Http\Request;
+use App\Models\Category;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 
@@ -27,12 +29,17 @@ class TopicsController extends Controller
 
 	public function create(Topic $topic)
 	{
-		return view('topics.create_and_edit', compact('topic'));
+        $categories = Category::all();
+		return view('topics.create_and_edit', compact('topic','categories'));
 	}
 
-	public function store(TopicRequest $request)
+	public function store(TopicRequest $request , Topic $topic)
 	{
-		$topic = Topic::create($request->all());
+        $topic->fill($request->all());
+		$topic->user_id = Auth::id();
+        dd($topic);
+        $topic->save();
+
 		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
 	}
 
